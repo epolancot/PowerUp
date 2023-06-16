31566ef5895cf6ba03e39ae14c96e95f6ab73684
Adam Buffett <adam_buffett@outlook.com>
2023-06-16 11:34:52 -0400

    Implement authenorization routes

--- powerup/urls.py
+++ powerup/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     # '' represents the "starts with" path
-    path('', include('main_app.urls')),
+    path("", include("main_app.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
