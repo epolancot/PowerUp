83393c5580ab845124b9e86ae0700d0334f9b0d6
Adam Buffett <adam_buffett@outlook.com>
2023-06-16 12:10:24 -0400

    Home and About routes

--- main_app/views.py
+++ main_app/views.py
@@ -7,6 +7,14 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 
+def home(request):
+    return render(request, "home.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
 def signup(request):
     error_message = ""
     if request.method == "POST":
